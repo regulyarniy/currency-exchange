27cecf04639aadc012bbc92f423eb4ff696c89a4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-20 22:52:14 +0000

    Deploy regulyarniy/currency-exchange to github.com/regulyarniy/currency-exchange.git:gh-pages

---
+++ precache-manifest.e68056d437a0bb2e6cc4e8e1efab65d8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dd119884f10e7dfbfa0",
+    "url": "/currency-exchange/static/js/runtime~main.4dd11988.js"
+  },
+  {
+    "revision": "f0b0e071e282a4c699e2",
+    "url": "/currency-exchange/static/js/main.f0b0e071.chunk.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "/currency-exchange/static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "89b02fd037e74e428e974b9a36e65f0b",
+    "url": "/currency-exchange/index.html"
+  }
+];
