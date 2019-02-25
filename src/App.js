94b37f02ca7c4918a604e7bdbfa14f6ae3cb8ace
Regulyarniy Nikolay <regularniy@gmail.com>
2019-02-25 14:20:27 +1000

    fix router basename

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Fragment>
           <CssBaseline/>
           <Header/>
