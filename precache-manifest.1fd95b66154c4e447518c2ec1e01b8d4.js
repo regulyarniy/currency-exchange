f7c0a3fc6176d5d63016054156015e7bcb8a452b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-26 07:40:34 +0000

    Deploy regulyarniy/currency-exchange to github.com/regulyarniy/currency-exchange.git:gh-pages

---
+++ precache-manifest.1fd95b66154c4e447518c2ec1e01b8d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4011a2c6f7fcab0ed1e",
+    "url": "/currency-exchange/static/js/runtime~main.d4011a2c.js"
+  },
+  {
+    "revision": "97c8d771b4e0820b7f9b",
+    "url": "/currency-exchange/static/js/main.97c8d771.chunk.js"
+  },
+  {
+    "revision": "2b90060168db0e71ba67",
+    "url": "/currency-exchange/static/js/5.2b900601.chunk.js"
+  },
+  {
+    "revision": "afc80e587c3ea279f5bc",
+    "url": "/currency-exchange/static/js/4.afc80e58.chunk.js"
+  },
+  {
+    "revision": "0fed9291412a59d0310f",
+    "url": "/currency-exchange/static/js/3.0fed9291.chunk.js"
+  },
+  {
+    "revision": "db8b3881dec492772db5",
+    "url": "/currency-exchange/static/js/2.db8b3881.chunk.js"
+  },
+  {
+    "revision": "ba8993008e88adab4eb4f38eeae638eb",
+    "url": "/currency-exchange/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/currency-exchange/precache-manifest.f9491220ce472557f9c8f3290884004a.js"
+  "/currency-exchange/precache-manifest.1fd95b66154c4e447518c2ec1e01b8d4.js"
 );
 
 workbox.clientsClaim();
