d62f3bf6ffb3de53811397bfa9f17fd4f442a292
Regulyarniy Nikolay <regularniy@gmail.com>
2019-05-25 11:23:39 +1000

    change api url

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const BASE_CURRENCY = `GBP`;
-export const RATES_URL = `https://ratesapi.io/api/latest?base=${BASE_CURRENCY}`;
+export const RATES_URL = `https://api.ratesapi.io/api/latest?base=${BASE_CURRENCY}`;
 export const LOCAL_SETTINGS = `settings`;
